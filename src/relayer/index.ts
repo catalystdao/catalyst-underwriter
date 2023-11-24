a8bc279ecfb3d36996ab2955c2bf8e3843d25491
Ran <51334627+RanCohenn@users.noreply.github.com>
2023-11-24 14:11:07 +0200

    cleanup unused method and chains

--- src/relayer/index.ts
+++ src/relayer/index.ts
@@ -4,14 +4,6 @@ import { AMB } from './interfaces/amb.interface';
 
 const baseEndpoint = process.env.RELAYER_ENDPOINT!;
 
-export const getBountyByID = async (id: string) => {
-  try {
-    const res = await axios.get(join(baseEndpoint, '/bounty/:id'));
-  } catch (error) {
-    console.error(`Failed to get bounty ${id} from the relayer`);
-  }
-};
-
 export const getAMBByID = async (id: string): Promise<AMB | undefined> => {
   try {
     const res = await axios.get<AMB>(join(baseEndpoint, '/amb/:id'));
@@ -22,17 +14,9 @@ export const getAMBByID = async (id: string): Promise<AMB | undefined> => {
   }
 };
 
-export const getAllBounties = async () => {
-  try {
-    const res = await axios.get(join(baseEndpoint, '/bounties'));
-  } catch (error) {
-    console.error(`Failed to get all bounties from the relayer`);
-  }
-};
-
 export const prioritise = async (id: string) => {
   try {
-    const res = await axios.post(join(baseEndpoint, '/prioritise/:id'));
+    await axios.post(join(baseEndpoint, '/prioritise/:id'));
   } catch (error) {
     console.error(`Failed to prioritise bounty ${id}`);
   }
