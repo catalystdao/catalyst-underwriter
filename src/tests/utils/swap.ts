b1b9f19b82d7e6176308eafc33c21b6f8299f167
Ran <51334627+RanCohenn@users.noreply.github.com>
2023-12-11 12:31:49 +0200

    changed encoding

--- src/tests/utils/swap.ts
+++ src/tests/utils/swap.ts
@@ -1,5 +1,11 @@
 import { ethers } from 'ethers';
-import { defaultAbiCoder, parseEther, parseUnits } from 'ethers/lib/utils';
+import {
+  defaultAbiCoder,
+  hexZeroPad,
+  parseEther,
+  parseUnits,
+  solidityPack,
+} from 'ethers/lib/utils';
 import { EvmChain } from '../../chains/evm-chain';
 import { Chain } from '../../chains/interfaces/chain.interface';
 import { MOCK_PRIVATE_KEY } from './constants';
@@ -22,7 +28,14 @@ export const swap = async (
     [fromChain.chainId],
   );
 
-  const toVault = toChain.catalystVault;
+  const toVault = solidityPack(
+    ['uint8', 'bytes32', 'address'],
+    [20, ethers.constants.HashZero, hexZeroPad(toChain.catalystVault, 32)],
+  );
+  const toAccount = solidityPack(
+    ['uint8', 'bytes32', 'address'],
+    [20, ethers.constants.HashZero, hexZeroPad(account, 32)],
+  );
   const incentive = {
     maxGasDelivery: 2000000,
     maxGasAck: 2000000,
@@ -55,7 +68,7 @@ export const swap = async (
     {
       chainIdentifier,
       toVault,
-      toAccount: account,
+      toAccount,
       incentive,
     },
     fromasset,
@@ -65,7 +78,7 @@ export const swap = async (
     account,
     underwriteIncentiveX16,
     cdata,
-    { gasLimit: 3000000, value: value },
+    { gasLimit: 3000000, value },
   );
 
   await tx.wait();
