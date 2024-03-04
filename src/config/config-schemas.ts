60697be5e604aa7d05e2565eb2693a24f75ff5fc
Jorge Sanmiguel <8038323+jsanmigimeno@users.noreply.github.com>
2024-03-04 18:18:49 +0100

    fix: Add 'startingBlock' config schema

--- src/config/config-schemas.ts
+++ src/config/config-schemas.ts
@@ -105,7 +105,8 @@ const LISTENER_SCHEMA = {
             type: "number",
             minimum: 0,
             maximum: 1_000_000,
-        }
+        },
+        startingBlock: {$ref: "positive-number-schema"},
     },
     additionalProperties: false
 }
